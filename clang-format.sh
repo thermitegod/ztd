2f07e408e20ad26334d8d8a604d41528108a2def
thermitegod <thermitethegod@gmail.com>
2022-02-09 12:48:04 -0700

    update clang-format.sh

--- clang-format.sh
+++ clang-format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find ./src -maxdepth 1 -o -iname *.hxx -o -iname *.cxx -o -iname *.ixx | xargs clang-format -i
+find ./src/* -maxdepth 1 -o -iname *.hxx -o -iname *.cxx -o -iname *.ixx | xargs clang-format -i
 

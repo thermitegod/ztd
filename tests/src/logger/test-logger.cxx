a570e59df86974f6992f6e49a0e797be82afb966
thermitegod <thermitethegod@gmail.com>
2022-05-29 18:21:48 -0700

    Logger tests

---
+++ tests/src/logger/test-logger.cxx
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2022 Brandon Zorn <brandonzorn@cock.li>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include "../../../src/ztd.hxx"
+#include "../../../src/ztd-logger.hxx"
+
+TEST(Logger, init)
+{
+    bool double_init_fail = false;
+
+    ztd::Logger::Init("test");
+    try
+    {
+        ztd::Logger::Init("test");
+    }
+    catch (const ztd::LoggerException& e)
+    {
+        double_init_fail = true;
+    }
+
+    ASSERT_TRUE(double_init_fail);
+}

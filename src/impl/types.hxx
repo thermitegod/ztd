4d8f9c2d31ea3632c9a4b18eab472a528691226b
thermitegod <thermitethegod@gmail.com>
2022-09-30 07:23:50 -0700

    re-enable custom types

--- src/impl/types.hxx
+++ src/impl/types.hxx
@@ -24,23 +24,28 @@
 
 #include <sys/types.h>
 
-// using i8    = int8_t;
-// using i16   = int16_t;
-// using i32   = int32_t;
-// using i64   = int64_t;
-// using i128  = __int128_t;
-//
-// using u8    = uint8_t;
-// using u16   = uint16_t;
-// using u32   = uint32_t;
-// using u64   = uint64_t;
-// using u128  = __uint128_t;
-//
-// using f32   = float;
-// using f64   = double;
-//
-// using usize = size_t;
-// using isize = ssize_t;
+// clang-format off
+
+// better type names
+using i8    = int8_t;
+using i16   = int16_t;
+using i32   = int32_t;
+using i64   = int64_t;
+using i128  = __int128_t;
+
+using u8    = uint8_t;
+using u16   = uint16_t;
+using u32   = uint32_t;
+using u64   = uint64_t;
+using u128  = __uint128_t;
+
+using f32   = float;
+using f64   = double;
+
+using usize = size_t;
+using isize = ssize_t;
+
+// clang-format on
 
 namespace ztd
 {

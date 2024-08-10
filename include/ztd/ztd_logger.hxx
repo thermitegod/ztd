8b44975077bf8157af76ee356ae98891f6eb4f5e
thermitegod <43489010+thermitegod@users.noreply.github.com>
2024-08-09 20:25:32 -0700

    simplify ztd::logger

--- include/ztd/ztd_logger.hxx
+++ include/ztd/ztd_logger.hxx
@@ -17,15 +17,9 @@
 
 #pragma once
 
-#include <string>
-#include <string_view>
-
 #include <filesystem>
 
 #include <memory>
-
-#include <cassert>
-#include <cstdlib>
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
@@ -35,70 +29,57 @@ namespace ztd::logger
 {
 namespace detail
 {
-struct manager;
-static inline std::shared_ptr<manager> Logger;
-struct manager
-{
-    static void
-    initialize(const spdlog::level::level_enum level, const std::filesystem::path& logfile) noexcept
-    {
-        ztd::logger::detail::Logger = std::make_shared<ztd::logger::detail::manager>();
-
-        std::vector<spdlog::sink_ptr> sinks;
-
-        const auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        console_sink->set_level(level);
-        console_sink->set_pattern(format.data());
-        sinks.push_back(console_sink);
-
-        if (!logfile.empty())
-        {
-            const auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logfile, true);
-            file_sink->set_level(level);
-            file_sink->set_pattern(format.data());
-            sinks.push_back(file_sink);
-        }
-
-        const auto logger =
-            std::make_shared<spdlog::logger>(ztd::logger::detail::manager::domain, sinks.cbegin(), sinks.cend());
-        logger->set_level(level);
-        logger->flush_on(level);
-
-        spdlog::register_logger(logger);
-
-        std::atexit(spdlog::shutdown);
-    }
-
-    static inline const std::string domain{"ztd"};
-    static inline const std::string_view format{"[%Y-%m-%d %H:%M:%S.%e] [%^%L%$] [thread %t] %v"};
-};
+static constexpr auto domain{"ztd"};
 } // namespace detail
 
 inline void
 initialize(const spdlog::level::level_enum level = spdlog::level::trace,
            const std::filesystem::path& logfile = "") noexcept
 {
-    detail::Logger->initialize(level, logfile);
+    const auto format{"[%Y-%m-%d %H:%M:%S.%e] [%^%L%$] [thread %t] %v"};
+
+    std::vector<spdlog::sink_ptr> sinks;
+
+    const auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_level(level);
+    console_sink->set_pattern(format);
+    sinks.push_back(console_sink);
+
+    if (!logfile.empty())
+    {
+        const auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logfile, true);
+        file_sink->set_level(level);
+        file_sink->set_pattern(format);
+        sinks.push_back(file_sink);
+    }
+
+    const auto logger = std::make_shared<spdlog::logger>(detail::domain, sinks.cbegin(), sinks.cend());
+    logger->set_level(level);
+    logger->flush_on(level);
+
+    spdlog::register_logger(logger);
+
+    std::atexit(spdlog::shutdown);
 }
 
 namespace utils
 {
 template<typename T>
 const void*
-ptr(T p)
+ptr(T p) noexcept
 {
     static_assert(std::is_pointer_v<T>);
     return (void*)p;
 }
 template<typename T>
 const void*
-ptr(const std::unique_ptr<T>& p)
+ptr(const std::unique_ptr<T>& p) noexcept
 {
     return (void*)p.get();
 }
 template<typename T>
 const void*
-ptr(const std::shared_ptr<T>& p)
+ptr(const std::shared_ptr<T>& p) noexcept
 {
     return (void*)p.get();
 }
@@ -106,97 +87,85 @@ ptr(const std::shared_ptr<T>& p)
 
 template<typename... Args>
 void
-trace(spdlog::format_string_t<Args...> fmt, Args&&... args)
+trace(spdlog::format_string_t<Args...> fmt, Args&&... args) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->trace(fmt, std::forward<Args>(args)...);
+    spdlog::get(detail::domain)->trace(fmt, std::forward<Args>(args)...);
 }
 
 template<typename... Args>
 void
-debug(spdlog::format_string_t<Args...> fmt, Args&&... args)
+debug(spdlog::format_string_t<Args...> fmt, Args&&... args) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->debug(fmt, std::forward<Args>(args)...);
+    spdlog::get(detail::domain)->debug(fmt, std::forward<Args>(args)...);
 }
 
 template<typename... Args>
 void
-info(spdlog::format_string_t<Args...> fmt, Args&&... args)
+info(spdlog::format_string_t<Args...> fmt, Args&&... args) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->info(fmt, std::forward<Args>(args)...);
+    spdlog::get(detail::domain)->info(fmt, std::forward<Args>(args)...);
 }
 
 template<typename... Args>
 void
-warn(spdlog::format_string_t<Args...> fmt, Args&&... args)
+warn(spdlog::format_string_t<Args...> fmt, Args&&... args) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->warn(fmt, std::forward<Args>(args)...);
+    spdlog::get(detail::domain)->warn(fmt, std::forward<Args>(args)...);
 }
 
 template<typename... Args>
 void
-error(spdlog::format_string_t<Args...> fmt, Args&&... args)
+error(spdlog::format_string_t<Args...> fmt, Args&&... args) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->error(fmt, std::forward<Args>(args)...);
+    spdlog::get(detail::domain)->error(fmt, std::forward<Args>(args)...);
 }
 
 template<typename... Args>
 void
-critical(spdlog::format_string_t<Args...> fmt, Args&&... args)
+critical(spdlog::format_string_t<Args...> fmt, Args&&... args) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->critical(fmt, std::forward<Args>(args)...);
+    spdlog::get(detail::domain)->critical(fmt, std::forward<Args>(args)...);
 }
 
 template<typename T>
 void
-trace(const T& msg)
+trace(const T& msg) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->trace(msg);
+    spdlog::get(detail::domain)->trace(msg);
 }
 
 template<typename T>
 void
-debug(const T& msg)
+debug(const T& msg) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->debug(msg);
+    spdlog::get(detail::domain)->debug(msg);
 }
 
 template<typename T>
 void
-info(const T& msg)
+info(const T& msg) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->info(msg);
+    spdlog::get(detail::domain)->info(msg);
 }
 
 template<typename T>
 void
-warn(const T& msg)
+warn(const T& msg) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->warn(msg);
+    spdlog::get(detail::domain)->warn(msg);
 }
 
 template<typename T>
 void
-error(const T& msg)
+error(const T& msg) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->error(msg);
+    spdlog::get(detail::domain)->error(msg);
 }
 
 template<typename T>
 void
-critical(const T& msg)
+critical(const T& msg) noexcept
 {
-    const auto logger = spdlog::get(ztd::logger::detail::manager::domain);
-    logger->critical(msg);
+    spdlog::get(detail::domain)->critical(msg);
 }
 } // namespace ztd::logger

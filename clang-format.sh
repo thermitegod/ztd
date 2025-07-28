9ecf593ea03787bf5f9760a7f264d804f3b74a2d
thermitegod <43489010+thermitegod@users.noreply.github.com>
2025-07-27 17:28:35 -0700

    update clang-format.sh

--- clang-format.sh
+++ clang-format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find ./include/ztd -type f | xargs clang-format -i
-find ./tests/src -type f | xargs clang-format -i
-find ./benchmarks/src -type f | xargs clang-format -i
+find ./include/ztd -iname '*.cxx' -o -iname '*.hxx' | xargs --max-args=$(nproc) clang-format -i
+find ./tests/src -iname '*.cxx' -o -iname '*.hxx' | xargs --max-args=$(nproc) clang-format -i
+find ./benchmarks/src -iname '*.cxx' -o -iname '*.hxx' | xargs --max-args=$(nproc) clang-format -i
